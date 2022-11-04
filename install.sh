fde031117d9a8305b8e984b0ab810bb45ebe1d7b
Tabonga Zifodya <zift@tessella.co.uk>
2022-11-04 10:34:41 +0000

    Install file commentary

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ cat > ./mosquitto/config/mosquitto.conf <<ENDOFFILE
 ###############Listener###############
 
 listener 1883 0.0.0.0
+### 1883 is the default port for Mosquitto,  0.0.0.0 means it will only run on the local network. No port forwarding is required.
+
 #cafile <path to ca file>
 #certfile <path to server cert>
 #keyfile <path to server key>
